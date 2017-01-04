a5da22fef5fa9921676ab0526e2c9ad1a050523c
Thy Nguyen <thyn88@live.com>
2017-01-04 15:09:56 -0500

    modified best.R

--- best.R
+++ best.R
@@ -3,7 +3,7 @@ best <- function(state, outcome){
     'Select variables of interest'
     data <- data[,c(2,7,11,17,23)]
     colnames(data)[3:5]<- c("heart.attack", "heart.failure", "pneumonia")
-    data[,3:5] <- lapply(data[,3:5], as.numeric)
+    data[,3:5] <- suppressWarnings(lapply(data[,3:5], as.numeric))
 
     'Check for invalid arguments'
     if (state %in% levels(as.factor(data$State)) == FALSE){
@@ -24,7 +24,6 @@ best <- function(state, outcome){
 
     data <- data[outcome_data[[outcome]]$data == outcome_data[[outcome]]$bestValue,]
     data <- data[is.na(data$Hospital.Name) == FALSE,]
-    sort(data$Hospital.Name)[1] 'Sort by hospital name and return first item'
+    sort(data$Hospital.Name)[1]
 }
 
-best("TX","hart attack")
