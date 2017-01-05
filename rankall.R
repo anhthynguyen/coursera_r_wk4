331b65b4fb31af1c3b9b7734b8af95dd6c37396a
Thy Nguyen <thyn88@live.com>
2017-01-05 17:21:28 -0500

    add rankall.R

---
+++ rankall.R
@@ -0,0 +1,34 @@
+rankall <- function(outcome, rank = "best"){
+    data <- read.csv("rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv", 
+                     na.strings="Not Available", 
+                     stringsAsFactors = FALSE)
+    
+    'Select variables of interest'
+    outcomes <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
+    data <- data[,c(2,7,outcomes[[outcome]])]
+    'rename columns'
+    colnames(data)<- c("hospital", "state", "outcome")
+    
+    'order data'
+    li <- order(data$state, data$outcome, data$hospital)
+    data <- data[li,]
+    
+    'remove NAs'
+    data <- data[complete.cases(data),]
+    
+    'handle data'
+    data <- split(data, data$state)
+    
+    fun <- function(x, rank){
+        if(rank == "best"){
+            return(x$hospital[1])
+        } else if(rank =="worst"){
+            return(x$hospital[length(x$hospital)])
+        } else {
+            return(x$hospital[rank])
+        }
+    }
+    
+    data <- sapply(data, fun, rank = rank)
+    data <- data.frame("hospital" = data, "state" = names(data))
+}
