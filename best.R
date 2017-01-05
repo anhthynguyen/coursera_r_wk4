949d1980438276768d2e7b4cd231843b52535a32
Thy Nguyen <thyn88@live.com>
2017-01-05 18:20:40 -0500

    fixed best.R

--- best.R
+++ best.R
@@ -1,29 +1,31 @@
 best <- function(state, outcome){
-    data <- read.csv("rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv", colClass="character")
-    'Select variables of interest'
-    data <- data[,c(2,7,11,17,23)]
-    colnames(data)[3:5]<- c("heart.attack", "heart.failure", "pneumonia")
-    data[,3:5] <- suppressWarnings(lapply(data[,3:5], as.numeric))
-
-    'Check for invalid arguments'
-    if (state %in% levels(as.factor(data$State)) == FALSE){
-        stop("invalid state")
-    }
+    'validate outcome'
     if (outcome %in% c("heart attack","heart failure", "pneumonia") == FALSE){
         stop("invalid outcome")
     }
-
-
-    data <- data[data$State == state,]
     
-    'List to return data for outcome of interest and best value'
+    'read data'
+    data <- read.csv("rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv", 
+                     na.strings="Not Available", 
+                     stringsAsFactors = FALSE)
     
-    outcome_data <- list("heart attack" = list("data" = data$heart.attack, "bestValue" = min(data$heart.attack, na.rm = TRUE)), 
-                         "heart failure" = list("data" = data$heart.failure, "bestValue" = min(data$heart.failure, na.rm = TRUE)), 
-                         "pneumonia" = list("data" = data$pneumonia, "bestValue" = min(data$pneumonia, na.rm=TRUE)))
+    'Select variables of interest'
+    outcomes <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
+    
+    'Check for invalid states'
+    if (state %in% unique(data$State) == FALSE){
+        stop("invalid state")
+    }
+    
+    data <- data[data$State == state, c(2,7,outcomes[[outcome]])]
+    'rename columns'
+    colnames(data)<- c("hospital", "state", "outcome")  
+    
+    data <- data[complete.cases(data),]
+    
+    best <- min(data$outcome)
 
-    data <- data[outcome_data[[outcome]]$data == outcome_data[[outcome]]$bestValue,]
-    data <- data[is.na(data$Hospital.Name) == FALSE,]
-    sort(data$Hospital.Name)[1]
+    data <- data[data$outcome == best,]
+    sort(data$hospital)[1]
 }
 
