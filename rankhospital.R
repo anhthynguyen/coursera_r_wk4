3ed530d65bf0ecbe5014dfa1c695a6f24fe3bb85
Thy Nguyen <thyn88@live.com>
2017-01-05 18:21:08 -0500

    fixed rankhospital.R

--- rankhospital.R
+++ rankhospital.R
@@ -1,36 +1,44 @@
 rankhospital <- function(state, outcome, num= "best"){
-    data <- read.csv("rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv", colClass="character")
-    'Select variables of interest'
-    data <- data[,c(2,7,11,17,23)]
-    colnames(data)[3:5]<- c("heart.attack", "heart.failure", "pneumonia")
-    data[,3:5] <- suppressWarnings(lapply(data[,3:5], as.numeric))
-    
-    'Check for invalid arguments'
-    if (state %in% levels(as.factor(data$State)) == FALSE){
-        stop("invalid state")
-    }
+    'validate outcome'
     if (outcome %in% c("heart attack","heart failure", "pneumonia") == FALSE){
         stop("invalid outcome")
     }
     
+    'read data'
+    data <- read.csv("rprog_data_ProgAssignment3-data/outcome-of-care-measures.csv", 
+                     na.strings="Not Available", 
+                     stringsAsFactors = FALSE)
 
-    data <- data[data$State == state,]
-    column <- list("heart attack" = data$heart.attack, "heart failure" = data$heart.failure, "pneumonia" = data$pneumonia)
+    'Select variables of interest'
+    outcomes <- c("heart attack" = 11, "heart failure" = 17, "pneumonia" = 23)
     
-    
-    li <- order(column[[outcome]], data$Hospital.Name)
+    'Check for invalid states'
+    if (state %in% unique(data$State) == FALSE){
+        stop("invalid state")
+    }
 
-    data <- data[!(is.na(column[[outcome]])),]    
+    data <- data[data$State == state, c(2,7,outcomes[[outcome]])]
+    'rename columns'
+    colnames(data)<- c("hospital", "state", "outcome")  
     
-    'Return conditions'
-    if (0 < num && num <= length(data$Hospital.Name)){
-        return (data[li,]$Hospital.Name[num])
+    data <- data[complete.cases(data),]
+
+    li <- order(data$outcome, data$hospital)
+
+       
+    data <- data[li,]
+
+    data$hospital    
+    
+    if(num == "best"){
+        return(data$hospital[1])
     } else if(num == "worst"){
-        return (data[li,]$Hospital.Name[length(data$Hospital.Name)])
-    } else if(num > length(data$Hospital.Name) || num < 0){
-        return (NA)
+        return(data$hospital[length(data$hospital)])
+    } else if(num > length(data$hospital) | num < 0){
+        return(NA)
     } else {
-        return (data[li,]$Hospital.Name[1])
+        return(data$hospital[num])
     }
 
 }
+
